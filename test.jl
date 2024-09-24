dde3d6218719b1700a79a669985784987268f241
Austin Holt <austin-holt@comcast.net>
2024-09-24 11:37:56 -0400

    added multithreading to test.jl

--- test.jl
+++ test.jl
@@ -1,13 +1,14 @@
 # benchmark.jl
 using BenchmarkTools
+using Base.Threads
 
 # Function to perform a simple computation
 function compute(n)
-    sum = 0.0
-    for i in 1:n
-        sum += sqrt(i)
+    sum = Threads.Atomic{Float64}(0.0)
+    @threads for i in 1:n
+        atomic_add!(sum, sqrt(i))
     end
-    return sum
+    return sum[]
 end
 
 # Benchmark the compute function and measure in ops/sec
