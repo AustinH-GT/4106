b154ea6a3ad87eb06db2de3b662daecb05e178fc
Austin Holt <austin-holt@comcast.net>
2024-09-24 11:26:51 -0400

    test.jl is now a benchmark

--- test.jl
+++ test.jl
@@ -1,36 +1,17 @@
-# test.jl
+# benchmark.jl
 
-# Print a simple message
-println("Hello, Julia!")
+using BenchmarkTools
 
-# Perform basic arithmetic
-a = 10
-b = 5
-println("a + b = ", a + b)
-println("a - b = ", a - b)
-println("a * b = ", a * b)
-println("a / b = ", a / b)
-
-# # Define a simple function
-# function greet(name)
-#     return "Hello, $name!"
-# end
-
-# println(greet("World"))
-
-# Test array operations
-arr = [1, 2, 3, 4, 5]
-println("Array: ", arr)
-println("Sum of array: ", sum(arr))
-
-# Test a simple loop
-for i in 1:5
-    println("Loop iteration: ", i)
+# Function to perform a simple computation
+function compute(n)
+    sum = 0.0
+    for i in 1:n
+        sum += sqrt(i)
+    end
+    return sum
 end
 
-# Test a conditional statement
-if a > b
-    println("a is greater than b")
-else
-    println("a is not greater than b")
-end
+# Benchmark the compute function
+n = 10^7
+println("Benchmarking compute function with n = $n")
+@btime compute($n)
